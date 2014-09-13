22921f4baadee1d152448498327d6e7192fc7e63
Marcus Hooper <hooperarma@gmail.com>
2014-09-13 17:08:15 +1000

    Fixed some layout issues

--- app/src/main/java/com/au/easyreference/app/Fragments/SearchDialog.java
+++ app/src/main/java/com/au/easyreference/app/Fragments/SearchDialog.java
@@ -1,14 +1,14 @@
 package com.au.easyreference.app.Fragments;
 
-import android.app.AlertDialog;
-import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -64,15 +64,14 @@ public class SearchDialog extends DialogFragment
 
 	private int visibleView = DOI;
 
+	@Nullable
 	@Override
-	public Dialog onCreateDialog(Bundle savedInstanceState)
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 	{
-		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+		getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
 
-		builder.setTitle(getString(R.string.search));
 		View layout = getActivity().getLayoutInflater().inflate(R.layout.search_dialog, null);
 		ButterKnife.inject(this, layout);
-		builder.setView(layout);
 
 		results = new ArrayList<Result>();
 
@@ -92,7 +91,7 @@ public class SearchDialog extends DialogFragment
 			}
 		});
 
-		return builder.create();
+		return layout;
 	}
 
 	private class TabClickListener implements View.OnClickListener
